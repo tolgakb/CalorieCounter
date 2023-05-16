d5fb68b9af3ca7f40fadcad113e8bdd1fd253278
tolgakb <tolgakabaahmet@gmail.com>
2023-05-16 17:14:15 +0100

    Project completion

--- caloriecounter/urls.py
+++ caloriecounter/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name= 'index'),
+    path('delete/<int:id>/', views.delete_consume, name= 'delete'),
+
 ]
